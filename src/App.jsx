302d46429e49fb6b08b2f84941450d49da6c4894
omnia-adnan <o.m.a.1514.ja@gmail.com>
2024-05-23 21:25:06 +0300

    create the project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className=" bg-indigo-950 h-screen">
+    <h1 className="text-8xl font-bold text-center	text-lime-400	">
+      Let's Begin
+    </h1>
+    </div>
+  );
+}
+
+export default App;
