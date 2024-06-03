7d0637862dc5279a4ee434901e0da9b3401c0d6b
omnia-adnan <o.m.a.1514.ja@gmail.com>
2024-06-03 21:13:37 +0300

    adding calnder page

--- src/Pages/Calendar.jsx
+++ src/Pages/Calendar.jsx
@@ -1,8 +1,15 @@
-function Calendar() {
+import { useState } from "react";
+import Calendar from "react-calendar";
+
+function CalendarPage() {
+    const [data, setData] = useState(new Date());
+    const onChang = newdata => {
+        setData(newdata);
+    }
     return(
-        <div className=" text-center 2xl:text-7xl">
-            hi Calendar
+        <div className=" text-white bg-blue-900 border border-black leading-6 2xl:text-7xl">
+            <Calendar onChang={onChang} value={data} />
         </div>
     );
 }
-export default Calendar;
+export default CalendarPage;
